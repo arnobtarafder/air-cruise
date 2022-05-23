98d092072a170e4e7fa4841a8bcffaffa3158923
arnobtarafder <rmtarnob@gmail.com>
2022-05-23 22:05:58 +0600

    add: users list section at admin dashboard

---
+++ src/Components/Dashboard/ADMIN/Users/Users.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import UserDeleteConfirmModal from "./UserDeleteConfirmModal";
+import UserRow from "./UserRow";
+
+const Users = () => {
+  const [deletingUser, setDeletingUser] = useState(null);
+  const {
+    data: users,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:5000/user", {
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  return (
+    <div className="px-10 py-10 bg-base-300 h-screen rounded-md">
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map((user, index) => (
+              <UserRow
+                index={index}
+                key={user._id}
+                user={user}
+                refetch={refetch}
+                setDeletingUser={setDeletingUser}
+              ></UserRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {deletingUser && (
+        <UserDeleteConfirmModal
+          deletingUser={deletingUser}
+          setDeletingUser={setDeletingUser}
+        ></UserDeleteConfirmModal>
+      )}
+    </div>
+  );
+};
+
+export default Users;
