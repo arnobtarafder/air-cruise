ffa996fe815e80a2703bdc229f25db8ccba14e24
arnobtarafder <rmtarnob@gmail.com>
2022-05-24 18:46:57 +0600

    add: fake data for porfolio projects

---
+++ src/FakeData/Projects.js
@@ -0,0 +1,66 @@
+const AllProjects = [
+    {
+      id: '097',
+      name: 'CHAYA',
+      category: 'full-stack',
+      photo:
+        'https://i.ibb.co/rwcCwTc/screencapture-bookchaya-2022-04-09-22-14-37-min-1-1-1.png',
+      description:
+        'CHAYA is Bangladesh’s first space sharing marketplace that connects Space owners with Users looking to find and book spaces for their needs. Here users can book stays for short term living or book a venues for party, photoshoot.',
+      technologies: [
+        { name: 'React' },
+        { name: 'Context API' },
+        { name: 'Tailwind' },
+        { name: 'Firebase' },
+        { name: 'NodeJS' },
+        { name: 'SSLCOMMERZ' },
+      ],
+      github: 'https://github.com/PriontoAbdullah',
+      website: 'https://www.bookchaya.com/',
+      status: 'featured',
+    },
+  
+    {
+      id: '098',
+      name: 'Anushilon - Virtual Science Lab',
+      category: 'full-stack',
+      photo: 'https://i.ibb.co/rc8wNNF/Oze0RSF.png',
+      description:
+        'ANUSILON is based on the idea that lab experiments can be taught using the internet, more efficiently and less expensively. This virtual lab available with no access to physical labs or where equipment is being scarce or costly.',
+      technologies: [
+        { name: 'React' },
+        { name: 'Redux' },
+        { name: 'Tailwind' },
+        { name: 'Express' },
+        { name: 'MongoDB' },
+        { name: 'Mongoose' },
+        { name: 'oAuth2' },
+      ],
+      github: 'https://github.com/PriontoAbdullah/Anushilon-Virtual-Science-Lab',
+      website: 'https://anushilon-bd.web.app/',
+      status: 'featured',
+    },
+    {
+      id: '098',
+      name: 'Anushilon - Virtual Science Lab',
+      category: 'full-stack',
+      photo: 'https://i.ibb.co/rc8wNNF/Oze0RSF.png',
+      description:
+        'ANUSILON is based on the idea that lab experiments can be taught using the internet, more efficiently and less expensively. This virtual lab available with no access to physical labs or where equipment is being scarce or costly.',
+      technologies: [
+        { name: 'React' },
+        { name: 'Redux' },
+        { name: 'Tailwind' },
+        { name: 'Express' },
+        { name: 'MongoDB' },
+        { name: 'Mongoose' },
+        { name: 'oAuth2' },
+      ],
+      github: 'https://github.com/PriontoAbdullah/Anushilon-Virtual-Science-Lab',
+      website: 'https://anushilon-bd.web.app/',
+      status: 'featured',
+    },
+  
+  ];
+  export default AllProjects;
+  
