74c4e9ae76e07929552e1fcf4a161d9d92d37b44
arnobtarafder <rmtarnob@gmail.com>
2022-05-26 23:47:03 +0600

    fix: firebase configaration hide

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,14 +4,24 @@ import { getAuth } from "firebase/auth";
 
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBLf1H1QVYioq68lpYxXwtPDFm-JGzHKEY",
+//   authDomain: "air-cruise.firebaseapp.com",
+//   projectId: "air-cruise",
+//   storageBucket: "air-cruise.appspot.com",
+//   messagingSenderId: "43938408952",
+//   appId: "1:43938408952:web:87925716d57745e316ce0d"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBLf1H1QVYioq68lpYxXwtPDFm-JGzHKEY",
-  authDomain: "air-cruise.firebaseapp.com",
-  projectId: "air-cruise",
-  storageBucket: "air-cruise.appspot.com",
-  messagingSenderId: "43938408952",
-  appId: "1:43938408952:web:87925716d57745e316ce0d"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
