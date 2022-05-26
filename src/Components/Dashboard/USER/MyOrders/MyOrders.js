1a4034406a963e29f20eff3b7a0fca1fe1630618
arnobtarafder <rmtarnob@gmail.com>
2022-05-26 17:47:02 +0600

    add: products data at json format

--- src/Components/Dashboard/USER/MyOrders/MyOrders.js
+++ src/Components/Dashboard/USER/MyOrders/MyOrders.js
@@ -26,9 +26,9 @@ const MyOrders = () => {
                 .then(res => {
                     console.log('res', res);
                     if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken');
-                        navigate('/');
+                        // signOut(auth);
+                        // localStorage.removeItem('accessToken');
+                        // navigate('/');
                     }
                     return res.json()
                 })
